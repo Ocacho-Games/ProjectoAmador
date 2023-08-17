ef5e378d52fdd5608fc05819b085d593746d2067
DavidOcacho <david.ocacho@gmail.com>
2023-08-17 18:28:41 +0200

    feat(core) [main]: Automatic Github Action for updating game projects

--- scripts/get_latest.bat
+++ scripts/get_latest.bat
@@ -1,3 +1,8 @@
+@REM Configuring Git in order to be able to push the changes
+git config --global pull.rebase false
+git config --global user.name DavidOcacho
+git config --global user.email david.ocacho@gmail.com
+
 git add .
 git stash save "Stashed things before get_latest"
 git fetch
@@ -6,3 +11,7 @@ git remote add template https://github.com/Ocacho-Games/Home-Godot
 git fetch --all
 git pull -X theirs template main --allow-unrelated-histories
 git remote remove template
+
+@REM Pushing changes
+git push
+
