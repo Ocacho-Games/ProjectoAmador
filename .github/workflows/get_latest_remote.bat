43b60ec525937b549eabd01fb208a9632c5a8e01
DavidOcacho <david.ocacho@gmail.com>
2023-08-18 13:26:10 +0200

    Changing get_latest_script

--- .github/workflows/get_latest_remote.bat
+++ .github/workflows/get_latest_remote.bat
@@ -1,3 +1,8 @@
+@REM Configuring Git in order to be able to push the changes
+git config --global pull.rebase false
+git config --global user.name DavidOcacho
+git config --global user.email david.ocacho@gmail.com
+
 git add .
 git stash save "Stashed things before get_latest"
 git fetch
@@ -9,3 +14,4 @@ git remote remove template
 
 @REM Pushing changes
 git push
+
