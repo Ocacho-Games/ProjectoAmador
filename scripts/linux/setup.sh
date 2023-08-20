d34f414e729adcc8891a40266e4dc68d5b44931f
DavidOcacho <david.ocacho@gmail.com>
2023-08-20 17:10:12 +0200

    feat(github) [main]: Scripts improved and now it should compile

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -1,27 +1,30 @@
+GIT_ROOT_PATH=$(git rev-parse --show-toplevel)
+DESTINATION_PATH=$GIT_ROOT_PATH/godot/
+
 echo == Updating Git Submodules ==
 git submodule update --remote 
 
-echo == Installing/Updating GUT (Godot Unit Testing) by Bitwes ==
+echo == Installing/Updating GUT [Godot Unit Testing] by Bitwes ==
 git clone -b godot_4 https://github.com/bitwes/Gut.git
-cp -rf Gut/addons ../godot/addons
+cp -rf Gut/addons $DESTINATION_PATH
 rm -rf Gut
 
 echo == Installing/Updating Scene Manager by Glass Brick ==
 git clone https://github.com/glass-brick/Scene-Manager
-cp -rf Scene-Manager/addons/ ../godot/addons
+cp -rf Scene-Manager/addons/ $DESTINATION_PATH
 rm -rf Scene-Manager
 
 echo == Installing/Updating Godot Sound Manager by Nathanhoad ==
 git clone https://github.com/nathanhoad/godot_sound_manager.git
-cp -rf godot_sound_manager/addons/ ../godot/addons
+cp -rf godot_sound_manager/addons/ $DESTINATION_PATH
 rm -rf godot_sound_manager
 
 echo == Installing/Updating Godot Debug Draw 3D by DmitriySalnikov ==
 git clone https://github.com/DmitriySalnikov/godot_debug_draw_3d.git
-cp -rf godot_debug_draw_3d/addons/ ../godot/addons
+cp -rf godot_debug_draw_3d/addons/ $DESTINATION_PATH
 rm -rf godot_debug_draw_3d
 
 echo == Installing/Updating Godot Steam by Gramps ==
 git clone -b gdextension-plugin https://github.com/CoaguCo-Industries/GodotSteam.git
-cp -rf GodotSteam/addons/ ../godot/addons
+cp -rf GodotSteam/addons/ $DESTINATION_PATH
 rm -rf GodotSteam
--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,30 +1,34 @@
 @echo off
 cls
 
+for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set "GIT_ROOT_PATH=%%i"
+
+set DESTINATION_PATH=%GIT_ROOT_PATH%\godot\addons\
+
 echo [92m== Updating Git Submodules ==
 git submodule update --remote 
 
 echo [94m== Installing/Updating GUT (Godot Unit Testing) by Bitwes == [90m
 git clone -b godot_4 https://github.com/bitwes/Gut.git
-xcopy Gut\addons ..\godot\addons /s /e /y
+xcopy Gut\addons "%DESTINATION_PATH%" /s /e /y
 rmdir /s /q Gut
 
 echo [94m== Installing/Updating Scene Manager by Glass Brick == [90m
 git clone https://github.com/glass-brick/Scene-Manager
-xcopy Scene-Manager\addons\ ..\godot\addons /s /e /y
+xcopy Scene-Manager\addons\ "%DESTINATION_PATH%" /s /e /y
 rmdir /s /q Scene-Manager
 
 echo [94m== Installing/Updating Godot Sound Manager by Nathanhoad == [90m
 git clone https://github.com/nathanhoad/godot_sound_manager.git
-xcopy godot_sound_manager\addons\ ..\godot\addons /s /e /y
+xcopy godot_sound_manager\addons\ "%DESTINATION_PATH%" /s /e /y
 rmdir /s /q godot_sound_manager
 
 echo [94m== Installing/Updating Godot Debug Draw 3D by DmitriySalnikov == [90m
 git clone https://github.com/DmitriySalnikov/godot_debug_draw_3d.git
-xcopy godot_debug_draw_3d\addons\ ..\godot\addons /s /e /y
+xcopy godot_debug_draw_3d\addons\ "%DESTINATION_PATH%" /s /e /y
 rmdir /s /q godot_debug_draw_3d
 
 echo [94m== Installing/Updating Godot Steam by Gramps == [90m
 git clone -b gdextension-plugin https://github.com/CoaguCo-Industries/GodotSteam.git
-xcopy GodotSteam\addons\ ..\godot\addons /s /e /y
+xcopy GodotSteam\addons\ "%DESTINATION_PATH%" /s /e /y
 rmdir /s /q GodotSteam
