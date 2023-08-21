d2a52fe0ef2e4365185e38cee8354d02718bcd94
DavidOcacho <david.ocacho@gmail.com>
2023-08-21 18:53:31 +0200

    Changing get latest script to not modify godot.project file

--- scripts/windows/get_latest.bat
+++ scripts/windows/get_latest.bat
@@ -1,4 +1,19 @@
-git add .
+set file=".gitignore"
+
+IF NOT EXIST %file% cd ..
+
+echo.>>.gitignore
+echo *.project>>.gitignore
+
+type .gitignore
+
+FOR /F "delims=*" %%g IN ('FINDSTR /R /I /V ".project" .gitignore') do echo %%g>>.gitignore_aux
+
+DEL /F .gitignore
+
+RENAME .gitignore_aux .gitignore
+
+@REM git add .
 git stash save "Stashed things before get_latest"
 git fetch
 git checkout dev
@@ -8,4 +23,4 @@ git pull -X theirs template main --allow-unrelated-histories
 git remote remove template
 
 @REM Pushing changes
-git push origin dev
+@REM git push origin dev
