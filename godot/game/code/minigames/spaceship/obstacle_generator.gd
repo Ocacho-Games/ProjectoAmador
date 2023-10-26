5280d95b2d5b00cb20fae925748be41f05690cbb
DavidOcacho <david.ocacho@gmail.com>
2023-10-26 17:57:35 +0200

    Spaceship minigame assets

--- godot/game/code/minigames/spaceship/obstacle_generator.gd
+++ godot/game/code/minigames/spaceship/obstacle_generator.gd
@@ -9,7 +9,7 @@ var timer : Timer = Timer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(timer)	
-	timer.wait_time = 0.5
+	timer.wait_time = 0.75
 	timer.connect("timeout", _on_timer_timeout)
 	timer.start()
 
@@ -21,12 +21,12 @@ func _on_timer_timeout():
 	var obstacle_node = obstacle.instantiate()
 	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	var obstacle_width = obstacle_node.get_child(0).texture.get_width()
-	obstacle_node.position.x = 300
-	obstacle_node.position.y = randf_range(-100, -500)
+	obstacle_node.position.x = randf_range(obstacle_width, screen_width - obstacle_width)
+	obstacle_node.position.y = -200
 	get_parent().add_child(obstacle_node)
 	
-	var obstacle_node2 = obstacle.instantiate()
-	obstacle_node.position.x = randf_range(obstacle_width * 2, screen_width - obstacle_width)
-	obstacle_node.position.y = randf_range(-100, -500)
-	get_parent().add_child(obstacle_node2)
+#	var obstacle_node2 = obstacle.instantiate()
+#	obstacle_node.position.x = randf_range(obstacle_width * 2, screen_width - obstacle_width)
+#	obstacle_node.position.y = randf_range(-100, -500)
+#	get_parent().add_child(obstacle_node2)
 
