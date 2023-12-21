2817dc85a8f6c4a605460e31159c4b28b59c3a6c
David Martínez García <126606809+DavidOcacho@users.noreply.github.com>
2023-12-21 16:33:51 +0100

    Supporting Linux (arch) and cleaning some libraries such as admob and gut

---
+++ scripts/linux/launch_godot_editor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+script_path=$(dirname $0)
+godot_url=https://downloads.tuxfamily.org/godotengine/4.1.1/Godot_v4.1.1-stable_linux.x86_64.zip
+
+if test -d $script_path/launch_godot_binaries; then
+     echo Godot is already downloaded. No need to download 
+else
+    mkdir $script_path/launch_godot_binaries
+    curl $godot_url -o $script_path/launch_godot_binaries/godot.zip
+    cd $script_path/launch_godot_binaries
+    unzip godot.zip
+    rm godot.zip
+
+fi
+
+# Calling the setup
+$script_path/setup.sh
+
+# Launching godot
+$script_path/launch_godot_binaries/Godot_v4.1.1-stable_linux.x86_64 -e --rendering-driver opengl3 --path $script_path/../../godot/ &
--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -1,42 +1,42 @@
 GIT_ROOT_PATH=$(git rev-parse --show-toplevel)
+
 DESTINATION_PATH=$GIT_ROOT_PATH/godot/
 
+LOCALIZATION_PATH=$GIT_ROOT_PATH/godot/game/localization
+LINK_LOCALIZATION_EXCEL=https://docs.google.com/spreadsheets/d/1_nm66MNoA1zymBRlSUbu-gO7j57r7ITFMtp_KpWK6FM/export?format=csv
+
+## Install the addon if not installed yet
+#1: Command for git after git clone
+#2: Name of the repository folder
+#3: Actual folder name of the plugin (The folder that appears on godot/addons)
+function install_plugin()
+{
+    if not test -d $DESTINATION_PATH/addons/$3; then
+        echo == Installing/Updating $2 ==
+        git clone $1
+        cp -rf $2/addons/ $DESTINATION_PATH
+        rm -rf $2
+    fi
+}
+
+# Updating Git submodules
 echo == Updating Git Submodules ==
 git submodule update --remote 
 
-echo == Installing/Updating GUT [Godot Unit Testing] by Bitwes ==
-git clone -b godot_4 https://github.com/bitwes/Gut.git
-cp -rf Gut/addons $DESTINATION_PATH
-rm -rf Gut
+# Updating localization
+echo [92m== Updating Localization ==
+wget $LINK_LOCALIZATION_EXCEL -O $LOCALIZATION_PATH/localization.csv > /dev/null 2>&1
 
-echo == Installing/Updating Scene Manager by Glass Brick ==
-git clone https://github.com/glass-brick/Scene-Manager
-cp -rf Scene-Manager/addons/ $DESTINATION_PATH
-rm -rf Scene-Manager
+# Installing addons
+install_plugin "-b godot_4 https://github.com/bitwes/Gut.git" Gut gut
+install_plugin https://github.com/nathanhoad/godot_sound_manager.git godot_sound_manager sound_manager 
+install_plugin https://github.com/glass-brick/Scene-Manager Scene-Manager scene_manager
+install_plugin https://github.com/DmitriySalnikov/godot_debug_draw_3d.git godot_debug_draw_3d debug_draw_3d
+install_plugin https://github.com/Poing-Studios/godot-admob-plugin.git godot-admob-plugin admob
 
-echo == Installing/Updating Godot Sound Manager by Nathanhoad ==
-git clone https://github.com/nathanhoad/godot_sound_manager.git
-cp -rf godot_sound_manager/addons/ $DESTINATION_PATH
-rm -rf godot_sound_manager
-
-echo == Installing/Updating Godot Debug Draw 3D by DmitriySalnikov ==
-git clone https://github.com/DmitriySalnikov/godot_debug_draw_3d.git
-cp -rf godot_debug_draw_3d/addons/ $DESTINATION_PATH
-rm -rf godot_debug_draw_3d
-
-# echo == Installing/Updating Godot Steam by Gramps ==
-# git clone -b gdextension-plugin https://github.com/CoaguCo-Industries/GodotSteam.git
-# cp -rf GodotSteam/addons/ $DESTINATION_PATH
-# rm -rf GodotSteam
-
-echo == Installing/Updating GodotTouchInputManager by Federico-Ciuffardi ==
-git clone -b godot4support https://github.com/Federico-Ciuffardi/GodotTouchInputManager.git
-mkdir $DESTINATION_PATH/touch_input_manager
-cp -rf GodotTouchInputManager/*.gd $DESTINATION_PATH/touch_input_manager
-rm -rf GodotTouchInputManager
-rm -rf $DESTINATION_PATH/touch_input_manager/.github
-
-echo == Installing/Updating Godot Admob by Gummaciel ==
-git clone https://github.com/Poing-Studios/godot-admob-plugin.git
-cp -rf godot-admob-plugin/addons/ $DESTINATION_PATH
-rm -rf godot-admob-plugin
+# Installing addons that don't follow the pattern
+if not test -d $DESTINATION_PATH/addons/touch_input_manager; then
+    echo == Installing/Updating GodotTouchInputManager by Federico-Ciuffardi ==
+    git clone -b godot4support https://github.com/Federico-Ciuffardi/GodotTouchInputManager.git
+    mv GodotTouchInputManager $DESTINATION_PATH/addons/touch_input_manager
+fi
